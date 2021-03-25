dacd48110aab495caf2852de8e604283ad398f25
filipe santos <fhilips.souza@gmail.com>
2021-03-25 17:45:32 -0300

    add card search structure

--- src/core/pages/search/CardSearch/index.tsx
+++ src/core/pages/search/CardSearch/index.tsx
@@ -1,9 +1,19 @@
 
+import ButtonIcon from '../../../components/ButtonIcon';
 import './styles.scss';
 
 const CardSearch = () => (
-    <div className="CardSearch-container">
-          <h1></h1>       
+    <div className="card-search-container">
+        <div className="card-search-content">
+            <h1>Encontre um perfil no Github</h1>
+
+            <input className="input-search-name" type="text" value="" placeholder="Digite o nome do usuário"/>
+            
+            <div className="btn-search">
+                <ButtonIcon text="ENCONTRAR" />
+            </div>
+            
+        </div>     
     </div>
     
 )
